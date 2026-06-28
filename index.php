39eb5973627b721eb244c2c9a2667d1e8987f103
Muhammad Yoga Prabowo <muhammadyogaprabowo@gmail.com>
2026-06-28 18:35:41 +0700

    Fix syntax error in CSS and add cache busting to index.php

--- index.php
+++ index.php
@@ -57,7 +57,7 @@ if (!in_array($page, $validPages)) $page = 'dashboard';
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css?v=1.0.1">
 </head>
 <body class="<?= $page === 'login' ? 'login-body' : '' ?>">
 
